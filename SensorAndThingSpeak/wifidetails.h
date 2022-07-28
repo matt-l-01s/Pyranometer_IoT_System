34b6d3d04d8865ebcf224de8aea1b55fb910099a
matt-l-01s <79315379+matt-l-01s@users.noreply.github.com>
2022-07-28 09:17:48 +0800

    Create wifidetails.h

---
+++ SensorAndThingSpeak/wifidetails.h
@@ -0,0 +1,12 @@
+// Download/copy paste this code then add in your details
+// Home WiFi details
+char homessid[] = "";
+char homepass[] = "";
+
+// Phone Hotspot details
+char hotssid[] = "";
+char hotpass[] = "";
+
+// Uni WiFi details
+char uniusername[] = ""; // Put your student ID here
+char unipass[] = ""; // Put your IIS password here
